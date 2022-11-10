331471c47f78bc5470da08a09a24a18dd36b9694
David Castro <arimus@arimac>
2022-11-10 01:42:23 -0700

    test non-default path mapping

--- src/__tests__/hapi.test.ts
+++ src/__tests__/hapi.test.ts
@@ -44,3 +44,48 @@ it('check basic query', async () => {
   await apolloServer.stop();
   await hapi.stop();
 });
+
+it('check non-default path', async () => {
+  const apolloServer = new ApolloServer({ typeDefs: 'type Query {f: ID}' });
+  await apolloServer.start();
+
+  const hapi = new Server({
+    debug: {
+      log: ['*'],
+      request: ['*']
+    },
+    host: 'localhost',
+    port: 5000
+  });
+
+  await hapi.register({
+    plugin: hapiPlugin,
+    options: {
+      apolloServer,
+      path: '/graphql'
+    }
+  });
+
+  await hapi.start();
+
+  const url = hapi.info.uri + '/graphql';
+  console.log('using non-default url', url);
+
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ query: '{f}' }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const statusCode = response.status;
+  const data = await response.text();
+
+  // console.log('DEBUG: status code', statusCode, data);
+  expect(statusCode).toEqual(200);
+  expect(data).toEqual('{"data":{"f":null}}\n');
+
+  await apolloServer.stop();
+  await hapi.stop();
+});
